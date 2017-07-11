508e1686f2c10cd01a1e35eb27b9d4feea64d0e6
bartgerard <bart.gerard@gmail.com>
2017-07-11 18:19:38 +0200

    + Mission + Team

---
+++ interface/src/main/java/be/gerard/escape/model/Team.java
@@ -0,0 +1,15 @@
+package be.gerard.escape.model;
+
+import lombok.Value;
+
+/**
+ * Team
+ *
+ * @author bartgerard
+ * @version v0.0.1
+ */
+@Value
+public class Team {
+    private final String name;
+    private final String password;
+}
